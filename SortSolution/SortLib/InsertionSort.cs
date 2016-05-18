af55e338ca1b5c78d42b923c7d85f089c25d6a5b
rpriisholm0 <rpriisholm@hotmail.com>
2016-05-18 14:11:23 +0200

    InsertionSort - Sort Finsihed

--- SortSolution/SortLib/InsertionSort.cs
+++ SortSolution/SortLib/InsertionSort.cs
@@ -15,7 +15,38 @@ namespace SortLib
 
         public T[] Sort<T>(T[] array) where T : IComparable, IComparable<T>
         {
-            throw new NotImplementedException();
+            List<T> result = new List<T>();
+            if (result.Count() > 0) {
+                result.Add(array[0]);
+
+                #region Iterates through each value in the array and puts the values ordered in the result list.
+                for (int i = 0; i < array.Count(); i++)
+                {
+                    T current = array[i];
+                    int lastIndex = result.Count() - 1;
+
+                    #region Find location in result (lowest value first)
+                    bool found = false;
+                    for (int j = 0; result.Count() < j && !found; j++)
+                    {
+                        if (result[j].CompareTo(current) < 0) {
+                            result.Insert(j, current);
+                        }
+                        else
+                        {
+                            if (lastIndex == j)
+                            {
+                                result.Add(current);
+                            }
+                        }
+                        
+                    }
+                    #endregion
+                }
+                #endregion
+            }
+
+            return result.ToArray();
         }
     }
 }
