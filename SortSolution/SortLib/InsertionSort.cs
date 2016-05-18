9aabb5cad83bba76523ea4e1873d871b88bc879d
rpriisholm0 <rpriisholm@hotmail.com>
2016-05-18 13:17:33 +0200

    Next2

--- SortSolution/SortLib/InsertionSort.cs
+++ SortSolution/SortLib/InsertionSort.cs
@@ -8,5 +8,14 @@ namespace SortLib
 {
     public class InsertionSort : ISort
     {
+        public T[] Merge<T>(T[] array_A, T[] array_B) where T : IComparable, IComparable<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] Sort<T>(T[] array) where T : IComparable, IComparable<T>
+        {
+            throw new NotImplementedException();
+        }
     }
 }
